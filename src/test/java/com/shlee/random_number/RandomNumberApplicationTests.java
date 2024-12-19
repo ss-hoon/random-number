40a83885d657de249d7e6728cc09d7a6fb47f65e
ss-hoon <lshoon1128@naver.com>
2024-12-19 21:30:44 +0900

    first commit

---
+++ src/test/java/com/shlee/random_number/RandomNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shlee.random_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
